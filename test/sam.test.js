a4b5e6d485219a0d5dfab8672b982910adcceb91
XGEN-IO\jdubray <jdubray@gmail.com>
2019-07-02 18:37:54 -0700

    Adds unit test

--- test/sam.test.js
+++ test/sam.test.js
@@ -3,39 +3,58 @@ import { expect } from "chai"
 import SAM from '../lib/SAM'
 import { step, E, first } from '../lib/sam-utils'
 
-describe('SAM tests', function() {
-  describe('loop', function() {
-    it('create an intent', function() {
-        SAM({
-            initialState: {
-                counter: 10,
-                status: 'ready'
-            }
-        })
+let tick = () => ({});
 
-        const { intents } = SAM({
+describe('SAM tests', function() {
+    before(function() {
+        tick = first(SAM({
             component: {
                 actions: [
                     () => ({test: true})
                 ]
             }
-        })
-        expect(first(intents)).to.exist 
-    });
+        }).intents)
+    }); 
 
-    it('add an acceptor', function() {
-        SAM({
-            component: {
-                acceptors: [
-                    model => ({ test }) => {
-                        if (test) {
-                          model.status = 'testing'
+    describe('loop', function() {
+        it('create an intent', function() {
+            SAM({
+                initialState: {
+                    counter: 10,
+                    status: 'ready'
+                }
+            })
+
+            expect(tick).to.exist 
+        });
+
+        it('add an acceptor', function() {
+            SAM({
+                component: {
+                    acceptors: [
+                        model => ({ test }) => {
+                            if (test) {
+                            model.status = 'testing'
+                            }
                         }
-                    }
-                ]
-            }, 
-            render: (state) => expect(state.status).to.equal('testing')
-        })
-    });
+                    ]
+                }, 
+                render: (state) => expect(state.status).to.equal('testing')
+            })
+
+            tick()
+        });
+
+        it('should add to the application state', function() {
+            SAM({
+                initialState: {
+                    warnings: []
+                },
+                render: (state) => expect(state.warnings).to.exist
+            })
+
+            tick()
+        });
+
   });
 });
