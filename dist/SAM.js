1ffffd2b68a09494ac2a835a9321bc46bfcdc292
XGEN-IO\jdubray <jdubray@gmail.com>
2019-07-07 21:40:33 -0700

    Adds model checker unit test

--- dist/SAM.js
+++ dist/SAM.js
@@ -67,7 +67,21 @@
     oneOf: triggered ? () => mon() : oneOf
   });
 
-  const clone = state => JSON.parse(JSON.stringify(state));
+  const clone = (state) => {
+    const comps = state.__components;
+    delete state.__components;
+    const cln = JSON.parse(JSON.stringify(state));
+    if (comps) {
+      cln.__components = []; 
+      if (comps.length > 0) {
+        comps.forEach((c) => {
+          delete c.parent;
+          cln.__components.push(Object.assign(clone(c), { parent: cln }));
+        });
+      }
+    }
+    return cln
+  };
 
   var timetraveler = (h = [], options = {}) => (function () {
     let currentIndex = 0;
@@ -126,6 +140,7 @@
     const naps = [];
     let logger;
     const { max } = O(options.timetravel);
+    const { hasAsyncActions = true } = options;
 
     // ancillary
     let renderView = () => null;
@@ -159,9 +174,15 @@
       }
     };
 
-    const display = (json, pretty) => {
+    const display = (json = {}, pretty) => {
       const keys = Object.keys(json);
-      return `{\n${keys.map(key => (key.index('__') === 0 ? '' : stringify(json[key], pretty))).join('\n')} }\n`
+      return `{\n${keys.map((key) => {
+      if (typeof key !== 'string') {
+        return ''
+      }
+      return key.indexOf('__') === 0 ? '' : stringify(json[key], pretty)
+    }).join('\n')
+    }\n}\n`
     };
 
 
@@ -173,11 +194,12 @@
       }
     };
 
-    const present = (proposal, privateState) => {
+    const present = async (proposal, privateState) => {
       // accept proposal
-      acceptors.forEach(accept(proposal));
+      const prop = await proposal;
+      acceptors.forEach(accept(prop));
 
-      storeBehavior(proposal);
+      storeBehavior(prop);
 
       // Continue to state representation
       state();
@@ -215,7 +237,15 @@
       }
 
       // Decorate actions to present proposal to the model
-      intents = A(component.actions).map(action => async (...args) => present(await action(...args)));
+      if (hasAsyncActions) {
+        intents = A(component.actions).map(action => (...args) => {
+          present(action(...args));
+        });
+      } else {
+        intents = A(component.actions).map(action => (...args) => {
+          present(action(...args));
+        });
+      }
 
       // Add component's acceptors,  reactors, naps and safety condition to SAM instance
       mount(acceptors, component.acceptors, component.localState);
@@ -236,12 +266,16 @@
     const setHistory = (h) => {
       history = timetraveler(h, { max });
       model.__hasNext = history.hasNext();
+      model.__behavior = [];
       renderView = wrap(_render, history.snap);
     };
 
     const timetravel = (travel = {}) => {
       if (E(history)) {
-        if (travel.reset) travel.index = 0;
+        if (travel.reset) {
+          travel.index = 0;
+          model.__behavior = [];
+        }
         if (travel.next) {
           model = history.next();
         } else if (travel.endOfTime) {
@@ -253,10 +287,11 @@
       renderView(model);
     };
 
-    const setCheck = ({ begin: { resolve }, end }) => {
-      if (E(resolve)) {
+    const setCheck = ({ begin = {}, end }) => {
+      const { render } = begin;
+      if (E(render)) {
         storeRenderView = renderView;
-        renderView = resolve;
+        renderView = render;
       }
 
       if (E(end)) {
@@ -294,6 +329,7 @@
 
   // A set of methods to use the SAM pattern
   var api = (SAM$1 = SAM) => ({
+    // Core SAM API
     addInitialState: initialState => SAM$1({ initialState }),
     addComponent: component => SAM$1({ component }),
     setRender: render => SAM$1({ render }),
@@ -302,14 +338,131 @@
     addReactors: (reactors, privateModel) => SAM$1({ component: { reactors, privateModel } }),
     addNAPs: (naps, privateModel) => SAM$1({ component: { naps, privateModel } }),
     addSafetyConditions: (safety, privateModel) => SAM$1({ component: { safety, privateModel } }),
+
+    // Time Travel
     addTimeTraveler: (history = []) => SAM$1({ history }),
     travel: (index = 0) => SAM$1({ travel: { index } }),
     next: () => SAM$1({ travel: { next: true } }),
     last: () => SAM$1({ travel: { endOfTime: true } }),
     hasNext: () => SAM$1({}).hasNext,
+    reset: () => SAM$1({ travel: { reset: true } }),
+
+    // Checker
     beginCheck: render => SAM$1({ check: { begin: { render } } }),
     endCheck: () => SAM$1({ check: { end: true } })
   });
+
+  const permutations = (arr, perms, currentDepth, depthMax) => {
+    const nextLevel = [];
+    if (perms.length === 0) {
+      arr.forEach(i => nextLevel.push([i]));
+    } else {
+      perms.forEach(p => arr.forEach((i) => {
+        const col = p.concat([i]);
+        nextLevel.push(col);
+      }));
+    }
+    currentDepth++;
+    if (currentDepth < depthMax) {
+      return permutations(arr, nextLevel, currentDepth, depthMax)
+    }
+    return nextLevel
+  };
+
+  const apply = (perms = [], reset) => {
+    perms.forEach((p) => {
+      let currentIndeces = [];
+      const indexMax = p.map(intent => A(O(intent).values).length);
+      const modMax = indexMax.map((val, index) => {
+        let out = 1;
+        for (let j = index; j < indexMax.length; j++) {
+          out *= indexMax[j];
+        }
+        return out
+      });
+      const increment = currentIndex => modMax.map(
+        (m, index) => {
+          if (index === modMax.length - 1) {
+            return currentIndex % indexMax[index]
+          }
+          return Math.floor(currentIndex / modMax[index + 1]) % indexMax[index]
+        }
+      );
+      for (let i = 0; i < p.length; i++) {
+        currentIndeces.push(0);
+      }
+      let k = 0;
+      const kmax = indexMax.reduce((acc, val) => acc * val, 1);
+      if (kmax === 0) {
+        throw new Error(['Checker: invalid dataset, one of the intents values has no value.',
+          'If an intent has no parameter, add an empty array to its values'].join('\n'))
+      }
+      do {
+        // eslint-disable-next-line no-loop-func
+        const vector = p.map((i, index) => i.values[currentIndeces[index]]);
+        // return to initial state
+        reset();
+        // apply behavior (intent(...values))
+        p.forEach((i, index) => {
+          const intentArgs = vector[index];
+          return i.intent(...intentArgs)
+        });
+        k++;
+        currentIndeces = increment(k);
+      } while (k < kmax)
+    });
+  };
+
+
+  const checker = ({
+    instance = SAM, intents = [], liveness, safety, depthMax = 5
+  }) => {
+    const { beginCheck, endCheck } = api(instance);
+    return new Promise((resolve, reject) => {
+      beginCheck((state) => {
+        liveness && liveness(state) && resolve(state);
+        safety && safety(state) && reject(state);
+      });
+      apply(
+        permutations(intents, [], 0, depthMax),
+        () => instance({ travel: { reset: true } })
+      );
+      endCheck();
+      reject(new Error('could not find liveness or safety conditions'));
+    })
+  };
+
+
+  // [a,b,c]
+  //  a
+  //  a      b          c
+  //  a b c  a b c      a b c
+  // aaa, aab, aac, aba, abb, abc, aca, acb, acc
+  // [[0,1], [0,1,2], [0,1,2,3]
+  // a(0) b(0) c(0)
+  // a(1) a(0) a(0)
+  // a(1) a(1) a(0)
+  // a(1) a(1) a(1)
+
+
+  // [[0,1], [0,1,2], [0,1,2,3]]
+  // 3 x 2 x 4 = 24
+
+  // for (k = 0; k < max[0]; k++) {
+  //    for(i = 0; i < max[1]; i++) {
+  //      for(j = 0; j<max[2] ) {
+  //         reset()
+  //         arg = [vals[k], vals[i], vals[j]]
+  //         p.forEach(i,index => i.intent(...args[index]))
+  //      }
+  //    }
+  // }
+
+  // int o = k+1 * i+1 * j+1
+  // 12 = % kmax * imax -> 6
+  // 4 = 0, 1, 0
+  // 8 = 0, 2 = o / max[col+1] % max[col], 0 = o % max[col]
+  // 12 = 1 = o / max[col+1] * max[col+2] % max[col], 0 = o / 4 % 3, 0 = o % max[col]
 
   // ISC License (ISC)
 
@@ -319,9 +472,12 @@
   } = api();
 
   var index = {
+    // Constructors
     SAM,
     createInstance,
     api,
+
+    // SAM Core
     addInitialState,
     addComponent,
     addAcceptors,
@@ -330,6 +486,8 @@
     addSafetyConditions,
     getIntents,
     setRender,
+
+    // Utils
     step,
     first,
     match,
@@ -337,7 +495,8 @@
     oneOf,
     utils: {
       O, A, N, NZ, S, F, E, or, and
-    }
+    },
+    checker
   };
 
   return index;
--- test/diehard.test.js
+++ test/diehard.test.js
@@ -5,20 +5,24 @@
 /* eslint-disable no-undef */
 const { expect } = require('chai')
 
+const { api, createInstance } = require('../dist/SAM')
+
 const { utils: { E, or } } = require('../dist/SAM')
 
-const {
-  api, createInstance
-} = require('../dist/SAM')
+const { checker } = require('../dist/SAM')
+
+const dieHarder = createInstance({ hasAsyncActions: false })
 
 const {
-  addInitialState, addComponent, setRender
-} = api(createInstance())
+  addInitialState, addComponent, setRender, addTimeTraveler
+} = api(dieHarder)
 
-let tick = () => ({})
+let checkerIntents = []
 
 describe('SAM examples: dieharder', () => {
   it('should initialize the jugs and the goal', () => {
+    addTimeTraveler()
+
     addInitialState({
       n: 2,
       jugs: [0, 0],
@@ -27,14 +31,14 @@ describe('SAM examples: dieharder', () => {
     })
   })
 
-  it('should add the juggler component', () => {
+  it('should add the juggler component and juggle to the goal', () => {
     const { intents } = addComponent({
       actions: [
         (j1, j2) => ({
-          jug2jug: { j1, j2 }
+          jug2jug: { j1, j2 }, __name: 'jug2jug'
         }),
-        j => ({ empty: j }),
-        j => ({ fill: j })
+        j => ({ empty: j, __name: 'empty' }),
+        j => ({ fill: j, __name: 'fill' })
       ],
       acceptors: [
         ({ jugs, capacity, n }) => ({ fill }) => {
@@ -50,12 +54,14 @@ describe('SAM examples: dieharder', () => {
         ({ jugs, capacity, n }) => ({ jug2jug }) => {
           if (E(jug2jug)) {
             const { j1, j2 } = jug2jug
-            if (E(j1) && j1 < n && j1 >= 0
-                && E(j2) && j2 < n && j2 >= 0) {
-              const maxAllowed = capacity[j2] - jugs[j2]
-              const transfer = Math.min(maxAllowed, jugs[j1])
-              jugs[j1] -= transfer
-              jugs[j2] += transfer
+            if (j1 !== j2) {
+              if (E(j1) && j1 < n && j1 >= 0
+                    && E(j2) && j2 < n && j2 >= 0) {
+                const maxAllowed = capacity[j2] - jugs[j2]
+                const transfer = Math.min(maxAllowed, jugs[j1])
+                jugs[j1] -= transfer
+                jugs[j2] += transfer
+              }
             }
           }
         }
@@ -63,8 +69,8 @@ describe('SAM examples: dieharder', () => {
     })
 
     let step = 0
-    setRender(({ goal, jugs }) => {
-      const goalReached = jugs.map(content => content === goal).reduce(or)
+    setRender(({ goal, jugs = [] }) => {
+      const goalReached = jugs.map(content => content === goal).reduce(or, false)
       // console.log(`Goal: ${goal} [${jugs.map(content => content).join(', ')}]`)
       // console.log( goalReached ? 'Goal reached!!!' : '')
       if (step === 5) expect(goalReached).to.be.true
@@ -83,5 +89,51 @@ describe('SAM examples: dieharder', () => {
     jug2jug(1, 0)
     fill(1)
     jug2jug(1, 0)
+
+    checkerIntents = [{
+      intent: (...args) => {
+        fill(...args)
+      },
+      name: 'fill',
+      values: [
+        [0],
+        [1]
+      ]
+    }, {
+      intent: (...args) => {
+        empty(...args)
+      },
+      name: 'empty',
+      values: [
+        [0],
+        [1]
+      ]
+    }, {
+      intent: (...args) => {
+        jug2jug(...args)
+      },
+      name: 'jug2jug',
+      values: [
+        [0, 1],
+        [1, 0]
+      ]
+    }
+    ]
   })
+
+  it('should check the model', async (done) => {
+    const behavior = await checker({
+      instance: dieHarder,
+      intents: checkerIntents,
+      liveness: ({ goal, jugs = [] }) => jugs.map(content => content === goal).reduce(or),
+      safety: ({ jugs = [], capacity = [] }) => jugs.map((content, index) => content < capacity[index]).reduce(or),
+    //   liveness: (state) => state.jugs.map(content => content === state.goal).reduce(or),
+    //   safety: (state) => state.jugs.map((content, index) => content < state.capacity[index]).reduce(or),
+      depthMax: 6
+    })
+
+    expect(behavior).to.exist
+
+    done()
+  }).timeout(60000)
 })
